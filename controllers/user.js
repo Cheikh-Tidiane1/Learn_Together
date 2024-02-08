bada43e06cc881a8a6aca2c181a604601bcb1c97
cheikh tidiane <cheikh951812@gmail.com>
2024-02-08 20:46:02 +0100

    Ajout de UserLogin

--- controllers/user.js
+++ controllers/user.js
@@ -1,20 +1,47 @@
-const bcrypt = require('bcrypt');
-const User = require('../models/User');
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
 
 exports.userSignUp = (req, res, next) => {
   bcrypt
     .hash(req.body.password, 10)
-    .then(hash => {
+    .then((hash) => {
       const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: hash,  
-        date_creation: new Date()
+        password: hash,
+        date_creation: new Date(),
       });
-      user.save()
+      user
+        .save()
         .then(() => res.status(201).json({ message: "Utilisateur crée" }))
         .catch((error) => res.status(400).json({ error }));
     })
     .catch((error) => res.status(500).json({ error }));
 };
-exports.userLogin = (req, res, next) => {};
+
+exports.userLogin = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(401)
+          .json({ message: "login/mot de passe incorrecte" });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res
+              .status(401)
+              .json({ message: "login/mot de passe incorrecte" });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: "TOKEN",
+            message: "User Exist"
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
