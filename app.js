76859cbe7a16d52f3dd2d1c04d5a9864580c227d
cheikh tidiane <cheikh951812@gmail.com>
2024-02-13 02:44:21 +0100

    Router: Cours

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const userRoutes = require('./routes/user');
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
+const courseRoutes = require("./routes/course");
 
-mongoose.connect('mongodb+srv://Learn_Cwh:QKvY07ebLXBckbPn@clusterlt.hvi44t0.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose
+  .connect(
+    "mongodb+srv://Learn_Cwh:QKvY07ebLXBckbPn@clusterlt.hvi44t0.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   next();
 });
 
-app.use('/wac/auth', userRoutes);
+app.use("/wac/course", courseRoutes);
+app.use("/wac/auth", userRoutes);
 
-module.exports = app ;
+module.exports = app;
---
+++ routes/course.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../middleware/auth");
+const coursCtrl = require("../controllers/course");
+
+router.post("/", coursCtrl.createCourse);
+router.get("/:id", coursCtrl.getOneCourse);
+router.get("/", coursCtrl.getAllCourse);
+router.delete("/:id", coursCtrl.deleteCourse);
+router.put("/:id", coursCtrl.modifyCourse);
+
+module.exports = router;
