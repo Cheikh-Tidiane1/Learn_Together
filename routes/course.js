b248bb0dd561e56f633bc87f49c1db52b0c65312
cheikh tidiane <cheikh951812@gmail.com>
2024-02-14 23:53:08 +0100

    Authentification des routes

--- routes/course.js
+++ routes/course.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
 const coursCtrl = require("../controllers/course");
 
-router.post("/", coursCtrl.createCourse);
-router.get("/:id", coursCtrl.getOneCourse);
-router.get("/", coursCtrl.getAllCourse);
-router.delete("/:id", coursCtrl.deleteCourse);
-router.put("/:id", coursCtrl.modifyCourse);
+router.post("/", auth, coursCtrl.createCourse);
+router.get("/:id", auth, coursCtrl.getOneCourse);
+router.get("/", auth, coursCtrl.getAllCourse);
+router.delete("/:id", auth, coursCtrl.deleteCourse);
+router.put("/:id", auth, coursCtrl.modifyCourse);
 
 module.exports = router;
