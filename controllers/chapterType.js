d049dff6b711bacec9c5c2d08e50ddfdd09c39bb
cheikh tidiane <cheikh951812@gmail.com>
2024-02-20 01:58:25 +0100

    Crud: chapitreType

---
+++ controllers/chapterType.js
@@ -0,0 +1,40 @@
+const ChapterType = require("../models/ChapterType");
+
+exports.createChapterType = (req, res, next) => {
+  delete req.body._id;
+
+  const chapterType = new ChapterType({
+    ...req.body,
+  });
+  chapterType
+    .save()
+    .then(() => res.status(201).json({ message: "Chapitre cré" }))
+    .catch((error) => res.status(500).json({ error }));
+};
+
+exports.deleteChapterType = (req, res, next) => {
+  ChapterType.deleteOne({ id: req.params.id })
+    .then(() => res.status(200).json({ message: "Chapitre supprimé" }))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.getAllChapterType = (req, res, next) => {
+  ChapterType.find()
+    .then((chapterType) => res.status(200).json(chapterType))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.getOneChapterType = (req, res, next) => {
+  ChapterType.findOne({ id: req.params.id })
+    .then((chapterType) => res.status(200).json(chapterType))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.modifyChapterType = (req, res, next) => {
+  ChapterType.updateOne(
+    { _id: req.params.id },
+    { ...req.body, _id: req.params.id }
+  )
+    .then(() => res.status(200).json({ message: "Chapitre modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
