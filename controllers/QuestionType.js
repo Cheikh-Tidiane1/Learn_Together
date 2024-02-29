f0936208a0676d1420477020fb2ca8f927592bf5
cheikh tidiane <cheikh951812@gmail.com>
2024-02-29 23:30:25 +0100

    QuestionType: CRUD

--- controllers/QuestionType.js
+++ controllers/QuestionType.js
@@ -8,6 +8,33 @@ exports.createQuestionType = (req, res, next) => {
 
   questionType
     .save()
-    .then(() => res.status(201).json({ message: "Question" }))
+    .then(() => res.status(201).json({ message: "Question " }))
     .catch((error) => res.status(500).json({ error }));
+};
+
+exports.deleteQuestionType = (req, res, next) => {
+  QuestionType.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Question supprimé" }))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.getAllQuestionType = (req, res, next) => {
+  QuestionType.find()
+    .then((QuestionType) => res.status(200).json(QuestionType))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.getOneQuestionType = (req, res, next) => {
+  QuestionType.findOne({ _id: req.params.id })
+    .then((QuestionType) => res.status(200).json(QuestionType))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.modifyQuestionType = (req, res, next) => {
+  QuestionType.updateOne(
+    { _id: req.params.id },
+    { ...req.body, _id: req.params.id }
+  )
+    .then(() => res.status(200).json({ message: "Question modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
 };
